d3b3ec74ce858df4851dacdb2b07d45e7031fcf1
A1ston <106008691+A1ston@users.noreply.github.com>
2022-05-24 21:42:17 +0800

    Update AutoOnlineDisk.bat

--- AutoOnlineDisk.bat
+++ AutoOnlineDisk.bat
@@ -9,12 +9,14 @@ if not exist .\listvolume.cfg fsutil file createnew listvolume.cfg 0&echo list v
 			@rem online disk
 			powershell -command "Get-Disk | Where-Object IsOffline -Eq $True | Set-Disk -IsOffline $False">nul
 		)
-		@rem check disk amount
+		@rem check disk amount and find disk1
 		set m=0
-		set s=3
-		set ltr=D
-		call :checkdisk_amount_ver2
-		
+		for /f "tokens=3" %%a in ('diskpart /s .\listvolume.cfg^|findstr /m /c:"Volume 2"') do set ltr=%%a
+		for /f %%a in ('"powershell ('FAT,NTFS' -match '%ltr%')"') do (
+			if %%a equ False set os=2012R2&set s=2&set ltr=D&set m=1
+			if %%a equ True set os=2019&set s=3&set ltr=D&set m=1
+			rem dubug disable echo [frist set] !s! !ltr! !m!
+		)
 		if %m% equ 1 call :checkdisk_amount_ver2
 			if "%m%" equ "" goto end
 		if %m% equ 2 call :checkdisk_amount_ver2
@@ -30,14 +32,16 @@ if not exist .\listvolume.cfg fsutil file createnew listvolume.cfg 0&echo list v
 		if %m% equ 7 call :checkdisk_amount_ver2
 			if "%m%" equ "" goto end
 		set m=
+		set s=
+		set ltr=
 		diskpart /s .\listvolume.cfg
 		echo   All Target disk has been online
 	
 		@rem scan_isReadOnly
 		echo check readonly state ..
-		set n=1
+		set n=0
 		call :check_readonly_loop
-		
+		if %n% equ 1 call :check_readonly_loop
 		if %n% equ 2 call :check_readonly_loop
 		if %n% equ 3 call :check_readonly_loop
 		if %n% equ 4 call :check_readonly_loop
@@ -68,7 +72,6 @@ if not exist .\listvolume.cfg fsutil file createnew listvolume.cfg 0&echo list v
 		powershell -command "set-disk -IsReadOnly $False"<.\value.cfg
 		goto :eof
 		
-		::powershell -command get-disk|findstr /c:"7      "
 		:checkdisk_amount_ver1
 		if %m% equ 7 (
 			powershell -command "(get-disk -Number !m!)|findstr /c:"!m!      " 
@@ -81,63 +84,82 @@ if not exist .\listvolume.cfg fsutil file createnew listvolume.cfg 0&echo list v
 		goto :eof
 		
 		:checkdisk_amount_ver2
-		if %m% equ 0 (
-			diskpart /s .\listvolume.cfg|findstr /m /c:"Volume 0">nul
+		@rem 2019
+		for /f %%a in ('"powershell ('%s%,%ltr%,%m%' -match '3,D,1')"') do if %%a equ True (
+			diskpart /s .\listvolume.cfg|findstr /m /c:"Volume 0">nul 
 			if !errorlevel! equ 0 (
 				diskpart /s .\listvolume.cfg|findstr /m /c:"Volume 1">nul
 				if !errorlevel! equ 0 (
 					diskpart /s .\listvolume.cfg|findstr /m /c:"Volume 2">nul
 					if !errorlevel! equ 0 (
-						echo Disk!m! Check OK
-						set /a m=!m!+1
-						goto :eof
+						echo Disk0 Check OK
 					)
 				)
 			)
 		)
 		
-		diskpart /s .\listvolume.cfg|findstr /m /c:"Volume %s%     %ltr%">nul
-		if %errorlevel% equ 1 (
-			for /f "tokens=3" %%a in ('diskpart /s .\listvolume.cfg^|findstr /m /c:"Volume !s!"') do set ltr=%%a
-			diskpart /s .\listvolume.cfg|findstr /m /c:"Volume !s!     !ltr!">nul
-			if !errorlevel! equ 1 goto checkdiskfail
+		@rem 2012
+		for /f %%a in ('"powershell ('%s%,%ltr%,%m%' -match '2,D,1')"') do if %%a equ True (
+			diskpart /s .\listvolume.cfg|findstr /m /c:"Volume 0">nul 
+			if !errorlevel! equ 0 (
+				diskpart /s .\listvolume.cfg|findstr /m /c:"Volume 1">nul
+				if !errorlevel! equ 0 (
+					echo Disk0 Check OK
+				)
 			)
+		)
+		
+		call :setltr
+		
+		diskpart /s .\listvolume.cfg|findstr /m /c:"%ltr%   %m%">nul
+		if !errorlevel! equ 1 goto checkdiskfail
 		if %errorlevel% equ 0 (
 			echo Disk!m! Check OK
-			set /a s=!s!+1
 			set /a m=!m!+1
-				if !ltr! equ F set ltr=G&goto :eof
-				if !ltr! equ G set ltr=H&goto :eof
-				if !ltr! equ H set ltr=I&goto :eof
-				if !ltr! equ I set ltr=J&goto :eof
-			@rem ltr+1 to hex convert upper
-			for /f %%a in ('"powershell [Convert]::ToString(0x!ltr!+1,16).ToUpper()"') do set ltr=%%a
+			goto :eof
 			)
-			goto :eof
-		
-		if %m% equ 7 (
-			diskpart /s .\listvolume.cfg|findstr /m /c:"Volume %s%     %ltr%">nul
-			if !errorlevel! equ 1 (
-				for /f "tokens=3" %%a in ('diskpart /s .\listvolume.cfg^|findstr /m /c:"Volume !s!"') do set ltr=%%a
-				if !errorlevel! equ 1 goto checkdiskfail
-				)
-			if !errorlevel! equ 0 echo disk!m! Check OK
-			set s=
-			set ltr=
-			goto :eof
-		)
-	
 		
 		:offline_disk
-		powershell -command "Get-Disk | Where-Object IsOffline -Eq $False | Set-Disk -IsOffline $True">nul
+		powershell "Get-Disk | Where-Object IsOffline -Eq $False | Set-Disk -IsOffline $True">nul
 		diskpart /s .\listdisk.cfg
 		pause
 		
+			
+		:setltr
+		if %m% equ 1 (
+		set ltr=D
+		goto :eof
+		)
+		if %m% equ 2 (
+		set ltr=E
+		goto :eof
+		)
+		if %m% equ 3 (
+		set ltr=F
+		goto :eof
+		)
+		if %m% equ 4 (
+		set ltr=G
+		goto :eof
+		)
+		if %m% equ 5 (
+		set ltr=H
+		goto :eof
+		)
+		if %m% equ 6 (
+		set ltr=I
+		goto :eof
+		)
+		if %m% equ 7 (
+		set ltr=J
+		goto :eof
+		)
+		
 		:checkdiskfail
 		color 0c
-		echo ===============================================
-		echo Disk "!m!" not found Volume %s%     Ltr=%ltr%   Label=%m% 
-		echo ===============================================
+		echo ==================================
+		echo Disk "%m%" not found Ltr=%ltr%   Label=%m%
+		echo ==================================
 		pause
 		start diskmgmt.msc
 		set m=
